267324d0b70e9738a4818d4d2ef078107e2d504e
Giltz254 <kiptoogodffrey68@gmail.com>
2025-05-09 16:34:38 +0300

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant={"ghost"} size={"lg"}>
+    <div className="flex flex-col items-center gap-8 w-full px-20">
+      <Button variant={"elevated"} size={"lg"}>
         Click Me
       </Button>
-      <p className="text-emerald-50 text-8xl font-bold">Good mornig</p>
+      <Input placeholder="Get in touch with me" />
+      <Progress value={60}/>
+      <Textarea />
+      <p className="text-emerald-700 text-xl font-bold">Good mornig</p>
     </div>
   );
 }
